285dbf1bd0ebbd4f78e525881dff7c2e3eddbecb
Balogun Razak <razak2free@gmail.com>
2023-04-27 10:15:45 +0100

    newchsnges

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,77 +1,98 @@
 import { useState } from "react";
 import { Menu, Button } from "antd";
 import {
-  HomeOutlined,
-  SettingOutlined,
-  UserOutlined,
-  SearchOutlined,
-  ShoppingCartOutlined,
-  MessageOutlined,
-  NotificationOutlined,
   AppstoreOutlined,
-  DashboardOutlined,
-  HeartOutlined,
-  MenuOutlined,
+  EditOutlined,
+  UsergroupAddOutlined,
+  HourglassOutlined,
+  CameraOutlined,
+  DeleteOutlined,
+  AppstoreAddOutlined,
+  FileAddOutlined,
+  ClockCircleOutlined,
 } from "@ant-design/icons";
+import styles from "./styles.module.css";
+
 const Sidebar = () => {
+  const [active, setActive] = useState("1");
   const [collapsed, setCollapsed] = useState(false);
- 
 
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
   };
 
+  const handleActive = (e: any) => {
+    setActive(e.target.id);
+  };
+
   return (
-    <div style={{  height: "90vh"}}>
-      <Menu mode="inline" theme="light" inlineCollapsed={collapsed}>
-        <div style={{ textAlign: "center", justifyContent: 'flex-start', display: 'flex', marginLeft: '20px',  marginTop: "38px", }}>
+    <div className={collapsed ? styles.togleAvtive : styles.sideBar}>
+      <div className={styles.navContent}>
+        <img src="/logo.svg" onClick={toggleCollapsed} />
+      </div>
+
+      <ul className={styles.navItem}>
         
-          <img
-            src="/logo.svg"
-            onClick={toggleCollapsed}
-            style={{
-              width: "40px",
-              height: "40px",
-              color: "#040404",
-              marginBottom: "50px",
-              cursor: "pointer",
-             
-            }}
-          />
-        </div>
-        <Menu.Item key="1" icon={<HomeOutlined />}>
-          Item 1
-        </Menu.Item>
-        <Menu.Item key="2" icon={<SettingOutlined />}>
-          Item 2
-        </Menu.Item>
-        <Menu.Item key="3" icon={<UserOutlined />}>
-          Item 3
-        </Menu.Item>
-        <Menu.Item key="4" icon={<SearchOutlined />}>
-          Item 3
-        </Menu.Item>
-        
-        <Menu.Item key="5" icon={<ShoppingCartOutlined />}>
-          Item 4
-        </Menu.Item>
-        <Menu.Item key="6" icon={<MessageOutlined />}>
-          Item 5
-        </Menu.Item>
-        
-        <Menu.Item key="7" icon={<NotificationOutlined />}>
-          Item 6
-        </Menu.Item>
-        <Menu.Item key="8" icon={<AppstoreOutlined />}>
-          Item 7
-        </Menu.Item>
-        <Menu.Item key="9" icon={<DashboardOutlined />}>
-          Item 8
-        </Menu.Item>
-        {/* <Menu.Item key="10" icon={<HeartOutlined />}>
-          Favorites
-        </Menu.Item> */}
-      </Menu>
+        <li
+          id="1"
+          className={active === "1" ? styles.navActive : styles.navList}
+          onClick={handleActive}
+        >
+          <EditOutlined /> <h5> Dashboard </h5>
+        </li>
+        <li
+          id="2"
+          className={active === "2" ? styles.navActive : styles.navList}
+          onClick={handleActive}
+        >
+          <UsergroupAddOutlined /> <h5> Item 2</h5>
+        </li>
+        <li
+          id="3"
+          className={active === "3" ? styles.navActive : styles.navList}
+          onClick={handleActive}
+        >
+          <HourglassOutlined /> <h5> Item 3</h5>
+        </li>
+        <p>Other 1</p>
+        <li
+          id="4"
+          className={active === "4" ? styles.navActive : styles.navList}
+          onClick={handleActive}
+        >
+          <CameraOutlined /> <h5> Item 4</h5>
+        </li>
+        <li
+          id="5"
+          className={active === "5" ? styles.navActive : styles.navList}
+          onClick={handleActive}
+        >
+          <DeleteOutlined /> <h5>Item 5</h5>
+        </li>
+        <p>Other 2</p>
+
+        <li
+          id="6"
+          className={active === "6" ? styles.navActive : styles.navList}
+          onClick={handleActive}
+        >
+          <AppstoreAddOutlined /> <h5>Item 6</h5>
+        </li>
+        <li
+          id="7"
+          className={active === "7" ? styles.navActive : styles.navList}
+          onClick={handleActive}
+        >
+          <FileAddOutlined /> <h5>Item 7</h5>
+        </li>
+        <li
+          id="8"
+          className={active === "8" ? styles.navActive : styles.navList}
+          onClick={handleActive}
+        >
+          <ClockCircleOutlined /> <h5>Item 8</h5>
+        </li>
+      </ul>
     </div>
   );
 };
